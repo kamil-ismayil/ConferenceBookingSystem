b54a9c64ebbdc718b3dbcc8a9f34f841044046a4
Your Name <nusret-24@hotmail.co.uk>
2020-05-15 13:45:25 +0200

    SṕlashScreenTimer & backbutton from login

--- app/src/main/java/com/conferencebookingsystem/MainActivity.java
+++ app/src/main/java/com/conferencebookingsystem/MainActivity.java
@@ -2,7 +2,9 @@ package com.conferencebookingsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        splashScreenTimer();
+    }
+
+    // efter 2 sekunder går den vidare till Login
+    public void splashScreenTimer() {
+        final Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this, Login.class));
+            }
+        }, 2000);
+
     }
 }
