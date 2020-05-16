6528981448184898e38eeaede72cc052467161ea
Kamil Ismayil <osl.kisma@gmail.com>
2020-05-16 20:04:14 +0200

    committing token/password creation

--- app/src/main/java/com/conferencebookingsystem/CreateAccount.java
+++ app/src/main/java/com/conferencebookingsystem/CreateAccount.java
@@ -1,9 +1,13 @@
 package com.conferencebookingsystem;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.math.BigInteger;
+import java.security.SecureRandom;
 
 public class CreateAccount extends AppCompatActivity {
 
@@ -11,5 +15,18 @@ public class CreateAccount extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_createaccount);
+
+        EditText passwordField = findViewById(R.id.txtPassword);
+        passwordField.setText(generateToken(8));
+
+    }
+
+    public String generateToken(int byteLength){
+
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] token = new byte[byteLength];
+        secureRandom.nextBytes(token);
+
+        return new BigInteger(1,token).toString(16);
     }
 }
--- app/src/main/java/com/conferencebookingsystem/Login.java
+++ app/src/main/java/com/conferencebookingsystem/Login.java
@@ -2,14 +2,36 @@ package com.conferencebookingsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+    Button buttonCreateAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        buttonCreateAccount = findViewById(R.id.btnCreateAccount);
+        buttonCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this, CreateAccount.class));
+            }
+        });
+
+
+//        buttonCreateAccount.setOnClickListener(new View().OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                startActivity(new Intent(Login.this, CreateAccount.class));
+//            }
+//        });
+
     }
 
     // Trycker ner programmet istället för att gå till splashscreen
@@ -18,5 +40,7 @@ public class Login extends AppCompatActivity {
         moveTaskToBack(true);
         //super.onBackPressed();
     }
-    
+
+
+
 }
