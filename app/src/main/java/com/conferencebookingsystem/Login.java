027af6fbf6aadc99d93f0083d3b22e4b6f6ec06c
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2020-05-19 12:31:22 +0200

    Create method to change activity when clicking textview field

--- app/src/main/java/com/conferencebookingsystem/Login.java
+++ app/src/main/java/com/conferencebookingsystem/Login.java
@@ -6,32 +6,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
 
-    Button buttonCreateAccount;
+    TextView txtCreateAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        buttonCreateAccount = findViewById(R.id.btnCreateAccount);
-        buttonCreateAccount.setOnClickListener(new View.OnClickListener() {
+        //Christian har skrivit den här metoden
+        txtCreateAccount = findViewById(R.id.txtCreateAccount);
+        txtCreateAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(Login.this, CreateAccount.class));
             }
         });
-
-
-//        buttonCreateAccount.setOnClickListener(new View().OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                startActivity(new Intent(Login.this, CreateAccount.class));
-//            }
-//        });
-
     }
 
     // Trycker ner programmet istället för att gå till splashscreen
