c2de23612763dcdf33b0f46c4553d1a49d72af72
Kamil Ismayil <osl.kisma@gmail.com>
2020-05-20 22:45:57 +0200

    Creat account, authentication and log in tasks are done. Committing.

--- app/src/main/java/com/conferencebookingsystem/CreateAccount.java
+++ app/src/main/java/com/conferencebookingsystem/CreateAccount.java
@@ -1,8 +1,14 @@
 package com.conferencebookingsystem;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +16,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
 
-import java.io.IOException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.math.BigInteger;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.security.SecureRandom;
 
@@ -22,6 +31,10 @@ public class CreateAccount extends AppCompatActivity {
 
     private Button buttonCreate;
     private RequestQueue mQueue;
+    AsyncTask<String, Void, String> asyncCreateUserAPI;
+    private TextView firstName, lastName, userName, password, email, phone, company, orgNo, street, zipCode, city;
+    private SharedPreferences userPassword;
+    private SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -29,21 +42,58 @@ public class CreateAccount extends AppCompatActivity {
         setContentView(R.layout.activity_createaccount);
 
         buttonCreate = findViewById(R.id.btnCreate);
-        EditText passwordField = findViewById(R.id.txtPassword);
+        final EditText passwordField = findViewById(R.id.txtPassword);
         passwordField.setText(generateToken(8));
+        userPassword = getSharedPreferences("customerPassword",MODE_PRIVATE);
+        editor = userPassword.edit();
 
         mQueue = Volley.newRequestQueue(this);
 
+        firstName = findViewById(R.id.txtFirstName);
+        lastName = findViewById(R.id.txtLastName);
+        userName = findViewById(R.id.txtUserName);
+        password = findViewById(R.id.txtPassword);
+        email = findViewById(R.id.txtEmail);
+        phone = findViewById(R.id.txtPhone);
+        company = findViewById(R.id.txtCompany);
+        orgNo = findViewById(R.id.txtOrgno);
+        street = findViewById(R.id.txtStreet);
+        zipCode = findViewById(R.id.txtZipcode);
+        city = findViewById(R.id.txtCity);
+        buttonCreate = findViewById(R.id.btnCreate);
+
+        asyncCreateUserAPI = new CreateAccountApi();
 
 
+        buttonCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editor.putString("password",password.getText().toString());
+                editor.commit();
+                asyncCreateUserAPI.execute("https://dev-be.timetomeet.se/service/rest/user/add/", jsonCreateUser());
+            }
+        });
     }
 
-    private void jsonParse(){
+    private String jsonCreateUser(){
 
+        String jsonCreateUser ="{" +
+            "    \"first_name\": " + "\"" + firstName.getText() + "\"," +
+            "    \"last_name\": " + "\"" + lastName.getText() + "\"," +
+            "    \"username\": " + "\"" + "z_".concat(userName.getText().toString()) + "\"," +
+            "    \"password\": " + "\"" + password.getText() + "\"," +
+            "    \"email\": " + "\"" + email.getText() + "\"," +
+            "    \"phone_number\": " + "\"" + phone.getText() + "\"," +
+            "    \"organization_name\": " + "\"" + company.getText() + "\"," +
+            "    \"organization_nr\": " + "\"" + orgNo.getText() + "\"," +
+            "    \"street\": " + "\"" + street.getText() + "\"," +
+            "    \"city_name\": " + "\"" + city.getText() + "\"," +
+            "    \"zipCode\": " + "\"" + zipCode.getText() + "\"" +
+            "}";
+        System.out.println("The whole json string is: " + jsonCreateUser);
 
-
+        return jsonCreateUser;
     }
-
 
     public String generateToken(int byteLength){
 
@@ -54,4 +104,67 @@ public class CreateAccount extends AppCompatActivity {
         return new BigInteger(1,token).toString(16);
     }
 
+    private class CreateAccountApi extends AsyncTask<String, Void, String> {
+        private String responseContent;
+        private String Error = null;
+        private JSONObject jsonObject;
+
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Toast.makeText(CreateAccount.this,"Creating account..",Toast.LENGTH_SHORT).show();
+        }
+
+        protected String doInBackground(String... requestData) {
+            BufferedReader reader=null;
+
+            try {
+                URL url = new URL(requestData[0]);  // url
+
+                HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+                connection.setRequestMethod("POST");
+                connection.setRequestProperty("Content-Type", "application/json");
+                connection.setRequestProperty("Accept","application/json");
+                connection.setDoOutput(true); // True för POST, PUT. False för GET
+
+                OutputStreamWriter wr = new OutputStreamWriter(connection.getOutputStream());
+                wr.write(requestData[1]); // { "username": ....
+                wr.flush();
+
+                reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuilder sb = new StringBuilder();
+                String line ;
+                while((line = reader.readLine()) != null) {
+                    sb.append(line + "  \n");
+                }
+
+                responseContent = sb.toString();
+                jsonObject = new JSONObject(responseContent);
+            }
+            catch(Exception ex) {
+                Error = ex.getMessage();
+            }
+            finally {
+                try {
+                    reader.close();
+                }
+                catch(Exception ex) {
+
+                }
+            }
+
+            return responseContent;
+        }
+
+        protected void onPostExecute(String result) {
+            try {
+                Thread.sleep(2000);
+                Toast.makeText(CreateAccount.this,"Account is created!",Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(CreateAccount.this,Login.class));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+        }
 }
--- app/src/main/java/com/conferencebookingsystem/Login.java
+++ app/src/main/java/com/conferencebookingsystem/Login.java
@@ -3,27 +3,50 @@ package com.conferencebookingsystem;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class Login extends AppCompatActivity {
 
     TextView txtCreateAccount;
 
+
+    private EditText txtPassword;
+    private EditText txtUsername;
+    AsyncTask<String, Void, String> asyncLoginAPI;
+    String token;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        txtPassword = findViewById(R.id.txtPassword);
+        txtUsername = findViewById(R.id.txtUserNamee);
+        SharedPreferences userPassword = getSharedPreferences("customerPassword",MODE_PRIVATE);
 
         Button start = (Button)findViewById(R.id.btnLogin);
+        final String password = userPassword.getString("password",null);
+
+        if(password != null){ txtPassword.setText(password); }
 
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                startActivity(new Intent(Login.this, Search.class));
+                asyncLoginAPI = new LoginApi();
+                asyncLoginAPI.execute("https://dev-be.timetomeet.se/service/rest/api-token-auth/", jsonUsernamePassword());
             }
         });
 
@@ -45,6 +68,85 @@ public class Login extends AppCompatActivity {
         //super.onBackPressed();
     }
 
+    private String jsonUsernamePassword(){
 
+        String jsonUsernamePassword ="{" +
+                "    \"username\": " + "\"" + txtUsername.getText() + "\"," +
+                "    \"password\": " + "\"" + txtPassword.getText() + "\"" +
+                "}";
+        System.out.println("The whole json string is: " + jsonUsernamePassword);
+
+        return jsonUsernamePassword;
+    }
+
+    private class LoginApi extends AsyncTask<String, Void, String> {
+        private String responseContent;
+        private String Error = null;
+
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Toast.makeText(Login.this,"Logging in..",Toast.LENGTH_SHORT).show();
+
+        }
+
+        protected String doInBackground(String... requestData) {
+            BufferedReader reader=null;
+
+            try {
+                URL url = new URL(requestData[0]);  // url
+
+                HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+                connection.setRequestMethod("POST");
+                connection.setRequestProperty("Content-Type", "application/json");
+                connection.setRequestProperty("Accept","application/json");
+                connection.setDoOutput(true); // True för POST, PUT. False för GET
+
+                OutputStreamWriter wr = new OutputStreamWriter(connection.getOutputStream());
+                wr.write(requestData[1]); // { "username": ....
+                wr.flush();
+
+                reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuilder sb = new StringBuilder();
+                String line ;
+                while((line = reader.readLine()) != null) {
+                    sb.append(line + "  \n");
+                }
+
+                responseContent = sb.toString();
+                System.out.println("The received token is: " + responseContent);
+                token = responseContent;
+            }
+            catch(Exception ex) {
+                Error = ex.getMessage();
+            }
+            finally {
+                try {
+                    reader.close();
+                }
+                catch(Exception ex) {
+
+                }
+            }
+
+            return responseContent;
+        }
+
+        protected void onPostExecute(String result) {
+
+            if(responseContent != null) {
+                try {
+                    Thread.sleep(2000);
+                    //Toast.makeText(Login.this, "Please wait!", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(Login.this, Search.class));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+//            else{
+//                Toast.makeText(Login.this,"Wrong username.Please try again..",Toast.LENGTH_SHORT).show();
+//            }
+
+        }
+    }
 
 }
