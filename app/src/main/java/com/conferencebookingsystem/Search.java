f057081351662013de939c43bcd418f093617981
Your Name <nusret-24@hotmail.co.uk>
2020-05-21 03:21:03 +0200

    Added a calandar in the search activity and some minor design changes

--- app/src/main/java/com/conferencebookingsystem/Search.java
+++ app/src/main/java/com/conferencebookingsystem/Search.java
@@ -2,27 +2,64 @@ package com.conferencebookingsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.List;
 
 public class Search extends AppCompatActivity {
+
+    private static final String tag = "Search";
+
+    private TextView Date;
+    private DatePickerDialog.OnDateSetListener DateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
+        Date = (TextView) findViewById(R.id.Date);
+
+        Date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog DatePicker = new DatePickerDialog(
+                        Search.this,
+                        android.R.style.Theme_Holo_Light_Dialog, DateListener, year, month, day);
+                DatePicker.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                DatePicker.show();
+
+            }
+        });
+
+        DateListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month = month + 1;
+                String selectedDate = dayOfMonth + "/" + month + "/" + year;
+                Date.setText(selectedDate);
+            }
+        };
 
         // https://android--code.blogspot.com/2015/08/android-spinner-hint.html
         final Spinner spinner = (Spinner) findViewById(R.id.CitySearch);
