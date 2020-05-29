e8419569e7ebdcfd38133643cb97e59834c29ca6
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2020-05-29 13:28:19 +0200

    Add new activity and start working on design

---
+++ app/src/main/java/com/conferencebookingsystem/Activities/Confirm.java
@@ -0,0 +1,16 @@
+package com.conferencebookingsystem.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.conferencebookingsystem.R;
+
+public class Confirm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm);
+    }
+}
